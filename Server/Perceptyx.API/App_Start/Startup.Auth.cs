3fcfdacdb756ebea9bdb36891363837217a53e69
rhanzies62 <francis.cebu@basecamptech.ph>
2016-07-22 10:09:01 +0800

    Initial Setup
    - added Web API Project
    - added core Project
    - added mobile app projects

---
+++ Server/Perceptyx.API/App_Start/Startup.Auth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace Perceptyx.API
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            PublicClientId = "self";
+            app.UseOAuthBearerTokens(OAuthOptions);
+        }
+    }
+}
